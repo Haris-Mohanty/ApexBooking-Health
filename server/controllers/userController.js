aa28846271e46f4f188245c941dcbc249d5de503
Haris Mohanty <harismohanty8658@gmail.com>
2024-02-03 14:35:08 +0530

    server/controller/userController.js- User registration successfully.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,42 @@
+import UserModel from "../Models/UserModel.js";
+import bcrypt from "bcrypt";
+
 export const register = async (req, res, next) => {
   try {
-    console.log("object")
+    const { name, email, password } = req.body;
+
+    //Validation
+    if (!name || !email || !password) {
+      return res.status(422).json({
+        message: "Please provide all fields!",
+      });
+    }
+    //Password validation
+    if (password.length < 6) {
+      return res.status(422).json({
+        message: "Password length should be greater than 6 character",
+      });
+    }
+
+    //Check existing user
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Email Already Exists!",
+      });
+    }
+
+    //Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+
+    //Register user
+    const user = new UserModel(req.body);
+    await user.save();
+    return res.status(201).json({
+      user,
+    });
   } catch (err) {
     return res.status(500).json({
       message: "Internal Server Error!",
