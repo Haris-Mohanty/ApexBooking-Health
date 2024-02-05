26a32074772673556aa94fe5c535bcfcb851f7ba
Haris Mohanty <harismohanty8658@gmail.com>
2024-02-05 15:24:37 +0530

    client/src/api/api.js- Make api request to server for login user.

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -15,3 +15,19 @@ export const registerUser = async (data) => {
     throw err;
   }
 };
+
+// ************* REGISTER USER **************/
+export const loginUser = async (data) => {
+  try {
+    const response = await axios.post("/auth/login", data);
+
+    if (response.status === 200) {
+      const resData = await response.data;
+      return resData;
+    } else {
+      throw new Error("Unexcepted Error Occured!");
+    }
+  } catch (err) {
+    throw err;
+  }
+};
