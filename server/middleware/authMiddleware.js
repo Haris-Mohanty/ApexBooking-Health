c186a8543f00dc0ee91a22f022316cdb0c7fae5e
Haris Mohanty <harismohanty8658@gmail.com>
2024-02-06 12:24:34 +0530

    server/middleware/authMiddleware.js- Create a middleware for get user id from token.

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).json({
+          message: "Auth Failed!",
+          success: false,
+        });
+      } else {
+        req.body.userId = decode.id;
+        next();
+      }
+    });
+  } catch (err) {
+    return res.status(401).json({
+      message: "Auth Failed!",
+      success: false,
+    });
+  }
+};
