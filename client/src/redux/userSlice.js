7f53a990dcdb860478d002a4927626a4d3383cfc
Haris Mohanty <harismohanty8658@gmail.com>
2024-02-14 23:00:10 +0530

    client- Reload user reducer function added.

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -4,12 +4,16 @@ export const userSlice = createSlice({
   name: "user",
   initialState: {
     user: null,
+    reloadUser: false,
   },
   reducers: {
     setUser: (state, actions) => {
       state.user = actions.payload;
     },
+    reloadUserData: (state, actions) => {
+      state.reloadUser = actions.payload;
+    },
   },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, reloadUserData } = userSlice.actions;
