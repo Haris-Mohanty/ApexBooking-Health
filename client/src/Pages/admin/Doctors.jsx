93579465e4026af59a0ec53a4fa8903e1b11a673
Haris Mohanty <harismohanty8658@gmail.com>
2024-02-19 16:21:26 +0530

    client- Ftech and show all users and doctors.

--- client/src/Pages/admin/Doctors.jsx
+++ client/src/Pages/admin/Doctors.jsx
@@ -1,10 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
+import { getAllDoctor } from "../../api/api";
+import { Table } from "antd";
 
 const Doctors = () => {
+  const [doctors, setDoctors] = useState([]);
+
+  //Fetch all doctors req
+  const fetchAllDoctors = async () => {
+    try {
+      const res = await getAllDoctor();
+      if (res.success) {
+        setDoctors(res.data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllDoctors();
+  }, []);
+
+  //Antd Table
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (text, record) => (
+        <span>
+          {record.firstName} {record.lastName}
+        </span>
+      ),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+    {
+      title: "Phone",
+      dataIndex: "phoneNumber",
+    },
+    {
+      title: "Department",
+      dataIndex: "department",
+    },
+    {
+      title: "Exp",
+      dataIndex: "experience",
+    },
+    {
+      title: "Fees",
+      dataIndex: "feePerConsultation",
+    },
+    {
+      title: "Created At",
+      dataIndex: "createdAt",
+      render: (createdAt) => new Date(createdAt).toLocaleDateString(),
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <div className="d-flex">
+          {record.status === "pending" ? (
+            <button className="btn btn-success">Apv.</button>
+          ) : (
+            <button className="btn btn-danger">Rej.</button>
+          )}
+        </div>
+      ),
+    },
+  ];
+
   return (
     <Layout>
-      <h1>All Doctors</h1>
+      <div className="table-responsive">
+        <h1>All Doctors</h1>
+        <Table
+          dataSource={doctors}
+          columns={columns}
+          rowKey={(record) => record._id}
+        />
+      </div>
     </Layout>
   );
 };
--- client/src/Pages/admin/Users.jsx
+++ client/src/Pages/admin/Users.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
 import { getAllUser } from "../../api/api";
+import { Table } from "antd";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -21,11 +22,47 @@ const Users = () => {
     fetchAllUsers();
   }, []);
 
-  console.log(users);
+  // antd table column
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Doctor",
+      dataIndex: "isDoctor",
+      render: (text, record) => <span>{record.isDoctor ? "Yes" : "No"}</span>,
+    },
+    {
+      title: "Created At",
+      dataIndex: "createdAt",
+      render: (createdAt) => new Date(createdAt).toLocaleDateString(),
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <div className="d-flex">
+          <button className="btn btn-danger">Block</button>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <Layout>
-      <h1>Users List</h1>
+      <div className="table-responsive">
+        <h1 className="mb-4 text-center mt-2">Users List</h1>
+        <Table
+          dataSource={users}
+          columns={columns}
+          rowKey={(record) => record._id}
+        />
+      </div>
     </Layout>
   );
 };
