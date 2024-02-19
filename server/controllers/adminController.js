d41675c993b7136f4f290c7d6f5ee3cbf60c84cb
Haris Mohanty <harismohanty8658@gmail.com>
2024-02-19 22:18:27 +0530

    server- Change doctor account status code fixed.

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -42,7 +42,42 @@ export const getAllDoctor = async (req, res, next) => {
 //********* CHANGE ACCOUNT STATUS (DOCTOR ACCOUNT) ********/
 export const changeAccountStatus = async (req, res) => {
   try {
-    
+    const { doctorId, status } = req.body;
+
+    const doctor = await DoctorModel.findByIdAndUpdate(doctorId, { status });
+    if (!doctor) {
+      return res.status(404).json({
+        success: false,
+        message: "Doctor not found.",
+      });
+    }
+
+    const user = await UserModel.findOne({ _id: doctor.userId });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found.",
+      });
+    }
+
+    const unSeenNotifications = user.unSeenNotifications;
+
+    //Push notification
+    unSeenNotifications.push({
+      type: "doctor-account-request-updated",
+      message: `Your doctor account request has ${status}.`,
+      onclickPath: "/notifications",
+    });
+
+    user.isDoctor === "approved" ? true : false;
+    user.save();
+
+    //Success res
+    return res.status(201).json({
+      success: true,
+      message: "Account status updated!",
+      data: doctor,
+    });
   } catch (err) {
     return res.status(500).json({
       success: false,
