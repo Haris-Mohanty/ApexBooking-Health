63836a93b50c211c27ffb623bb1553de2cc9fdcf
Haris Mohanty <harismohanty8658@gmail.com>
2024-02-23 23:16:18 +0530

    client- Get user successfully.

--- client/src/Pages/doctor/DoctorProfile.jsx
+++ client/src/Pages/doctor/DoctorProfile.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout";
+import { useSelector } from "react-redux";
 
 const DoctorProfile = () => {
+  const { user } = useSelector((state) => state.user);
+  const [doctor, setDoctor] = useState(null);
+
+  
   return (
     <>
       <Layout>
