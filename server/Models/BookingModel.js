583a26a0eef09c3e57d468832459986b602c6d5c
Haris Mohanty <harismohanty8658@gmail.com>
2024-02-29 22:53:16 +0530

    server- Model created for booking appointments.

---
+++ server/Models/BookingModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    doctorId: {
+      type: String,
+      required: true,
+    },
+    doctorInfo: {
+      type: String,
+      required: true,
+    },
+    userInfo: {
+      type: String,
+      required: true,
+    },
+    data: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "pending",
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Booking", bookingSchema);
