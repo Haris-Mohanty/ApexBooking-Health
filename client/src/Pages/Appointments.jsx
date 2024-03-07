24f524adf421af26564e60be5de581d214b08181
Haris Mohanty <harismohanty8658@gmail.com>
2024-03-07 17:59:59 +0530

    client- Appointments showing successfully.

--- client/src/Pages/Appointments.jsx
+++ client/src/Pages/Appointments.jsx
@@ -3,11 +3,14 @@ import Layout from "../components/Layout";
 import { getUserAppointments } from "../api/api";
 import { useDispatch } from "react-redux";
 import { hideLoading, showLoading } from "../redux/spinnerSlice";
+import moment from "moment";
+import { Table } from "antd";
 
 const Appointments = () => {
   const [appointments, setAppointments] = useState([]);
   const dispatch = useDispatch();
 
+  // Fetch User Appointments
   const fetchUserAppointments = async () => {
     try {
       dispatch(showLoading());
@@ -25,10 +28,46 @@ const Appointments = () => {
     fetchUserAppointments();
   }, []);
 
+  //Create antd table
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "_id",
+    },
+    // {
+    //   title: "Name",
+    //   dataIndex: "name",
+    //   render: (text, record) => (
+    //     <span>
+    //       {record.doctorid.firstName} {record.doctorId.lastName}
+    //     </span>
+    //   ),
+    // },
+    // {
+    //   title: "Phone",
+    //   dataIndex: "phone",
+    //   render: (text, record) => <span>{record.doctorId.phone}</span>,
+    // },
+    {
+      title: "Date & Time",
+      dataIndex: "date",
+      render: (text, record) => (
+        <span>
+          {moment(record.date).format("DD-MM-YYYY")} &nbsp;
+          {moment(record.time).format("HH:mm")}
+        </span>
+      ),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+  ];
+
   return (
     <Layout>
-      <h1>Appointments List</h1>
-      {/* <div>{appointments.time}</div> */}
+      <h1 className="m-4 text-center">Appointments List</h1>
+      <Table className="mt-3" columns={columns} dataSource={appointments} />
     </Layout>
   );
 };
