4263ec1edd9a01bb06d76a5c77551715216f3a35
Haris Mohanty <harismohanty8658@gmail.com>
2024-03-14 22:27:33 +0530

    client- Show user profile details code processing...

---
+++ client/src/Pages/UserProfile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const UserProfile = () => {
+  return (
+    <Layout>
+      <h1>Profile Info</h1>
+    </Layout>
+  );
+};
+
+export default UserProfile;
